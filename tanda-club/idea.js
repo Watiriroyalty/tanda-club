0b98aa9766ba4d6d8d018e52d8bbaec3d142c88f
watiriroyalty <watirikambo@gmail.com>
2021-11-13 10:42:12 +0300

    The Blueprint

---
+++ tanda-club/idea.js
@@ -0,0 +1,12 @@
+/*
+
+//WATIRI
+The main idea is to create a rotating savings club where 
+each participant contributes a certain amount of money(token) after a certain period of time.
+
+
+
+
+
+
+*/
